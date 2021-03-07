bee4aab83e1494b2ef1c00a3502b9ac11a6db844
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-03-08 00:06:28 +0700

    C4 Initial circle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-let canvas = document.querySelector("canvas");
-canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+let canvas = document.querySelector('canvas')
+canvas.width = document.body.clientWidth
+canvas.height = document.body.clientHeight
 
-console.log(canvas);
-const pen = canvas.getContext("2d");
+console.log(canvas)
+const pen = canvas.getContext('2d')
 
 class Circle {
-  constructor(x, y, dx, dy, radius, color) {}
+  constructor(x, y, dx, dy, radius, color) {
+    this.x = x
+    this.y = y
+    this.dx = dx
+    this.dy = dy
+    this.radius = radius
+    this.color = color
+    this.setup()
+  }
+
+  setup = () => {
+    pen.beginPath()
+    pen.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    pen.fillStyle = this.color
+    pen.strokeStyle = this.color
+    pen.fill()
+  }
+
+  update = () => {
+    if(this.x + this.radius > canvas.width || this.x)
+    
+    this.setup()
+
+  }
 }
+
+const circle = new Circle(50, 50, 5, 5, 30, '#11ffaa')
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,47 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "src", "index"),
+  mode: 'development',
+  entry: path.join(__dirname, 'src', 'index'),
   watch: true,
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/dist/",
-    filename: "bundle.[hash].js",
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.[hash].js',
   },
   target: 'node',
   module: {
     rules: [
       {
         test: /.jsx?$/,
-        include: [path.resolve(__dirname, "src")],
-        exclude: [path.resolve(__dirname, "node_modules")],
-        loader: "babel-loader",
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env"],
+          presets: [
+            '@babel/preset-env',
+          ],
         },
       },
     ],
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: "./src/template.html",
-      title: "Collision Canvas",
+      template: './src/template.html',
+      title: 'Collision Canvas',
     }),
     new CleanWebpackPlugin(),
   ],
   resolve: {
-    extensions: [".json", ".js", ".jsx"],
+    extensions: ['.json', '.js', '.jsx'],
   },
   // devtool: "source-map",
   devServer: {
-    contentBase: path.join(__dirname, "/dist/"),
+    contentBase: path.join(__dirname, '/dist/'),
     inline: true,
-    host: "localhost",
+    host: 'localhost',
     port: 5000,
   },
-};
+}
