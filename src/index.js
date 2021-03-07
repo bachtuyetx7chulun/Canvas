52d8141e3d29a251f72f4169962aab84d1d4be18
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-03-07 23:38:43 +0700

    C3 Setup webpack completed

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector("canvas");
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+
+console.log(canvas);
+const pen = canvas.getContext("2d");
+
+class Circle {
+  constructor(x, y, dx, dy, radius, color) {}
+}
